098add9a9adc71045d553ed8c6f1273ded280b68
Damian Lee <damianlee84@gmail.com>
2016-02-01 16:36:55 -0500

    Damian's Second Commit.

--- ASH/mainApp/views.py
+++ ASH/mainApp/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("I hope this works. (Damian)")
+    return HttpResponse("Ok, my first commit worked! Let's see if my second commit works...(Damian)")
