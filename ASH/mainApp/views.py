3eb8de84e9c5b11dcd9952074c8a5af724129bad
Pablo Prieto <prietop.pablo@gmail.com>
2016-02-03 21:50:10 -0500

    added more views for testing.

--- ASH/mainApp/views.py
+++ ASH/mainApp/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Main page.")
+
+def detail(request, user_id):
+    return HttpResponse("You're looking at user %s." % user_id)
+
+def results(request, user_id):
+    response = "You're looking at the results of user %s."
+    return HttpResponse(response % user_id)
+
+def vote(request, user_id):
+    return HttpResponse("You're voting on user %s." % user_id)
