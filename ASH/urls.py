37b227d99981333fabcdb786a0ac82b138515484
Damian Lee <damianlee84@gmail.com>
2016-02-28 18:03:30 -0500

    Rescrutctured the project directory hierarchy (specially for the static and media) and added all the configurations to be able deploy the project.

---
+++ ASH/urls.py
@@ -0,0 +1,37 @@
+"""ASH URL Configuration.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^mainApp/', include('mainApp.urls')),
+    url(r'^admin/', admin.site.urls),
+
+    # Redirect page when /mainApp/ is not included in the address
+    url(r'^$', RedirectView.as_view(url='/mainApp/'), name='index'),
+]
+
+# This makes sure this applies only development environment
+# If you run django server with DEBUG=True, server will serve static files straight form STATICFILES_DIRS,
+# and if DEBUG=False it won't handle static files at all
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ASH.wsgi --log-file -
