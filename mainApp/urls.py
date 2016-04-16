58f7b27f130004b57c672753ded410578c9c02ea
Pablo Prieto <prietop.pablo@gmail.com>
2016-04-16 18:55:46 -0400

    Fixing media files

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -18,4 +20,4 @@ urlpatterns = [
     url(r'^(?P<user_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<user_id>[0-9]+)/vote/$', views.vote, name='vote'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
