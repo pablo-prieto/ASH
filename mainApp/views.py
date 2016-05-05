1b31d179a8bf83e9d124cbe423b1af658b75996f
Your Name <you@example.com>
2016-05-05 15:55:26 -0400

    Memories passed successfully to the backend.

--- mainApp/views.py
+++ mainApp/views.py
@@ -224,10 +224,10 @@ def addMemory(request):
                 mem_descp = request.POST.get('mem-descrip-input')
                 date_start = request.POST.get('input-datestart')
                 date_end = request.POST.get('input-dateend')
-                input_field = request.POST.get('uploadBtn1')
-
-                print request.POST
-                form_data = json.dumps({'context': mem_title})
+                print request.POST.keys()
+                print
+                print request.FILES.keys()
+                form_data = json.dumps({'context': "mem_title"})
                 return HttpResponse(form_data)
 
             except KeyError:
